aca3db6b72eb27a7b990f1c17bacc3f156cfeb9c
Samet Kalkan <abdulsametkalkan@gmail.com>
2019-04-03 23:19:10 +0300

    word level train added

---
+++ train_word_level.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+from keras.callbacks import LambdaCallback
+from keras.models import Sequential
+from keras.layers import Dense, Embedding
+from keras.layers import LSTM
+from keras.optimizers import RMSprop
+from keras.utils.data_utils import get_file
+import numpy as np
+import random
+import sys
+import io
+from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical
+import utils
+
+from numpy import array
+from pickle import dump
+import time
+
+lines = utils.load_doc("a_tenth_data_sequences.txt").split('\n')
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(lines)
+sequences = tokenizer.texts_to_sequences(lines)
+
+vocab_size = len(tokenizer.word_index) + 1
+
+sequences = array(sequences)
+X, y = sequences[:, :-1], sequences[:, -1]
+
+y = to_categorical(y, num_classes=vocab_size)  # ------- MemoryError bu satırda
+
+seq_length = X.shape[1]
+
+#
+# model = Sequential()
+# model.add(Embedding(vocab_size, 50, input_length=seq_length))
+# model.add(LSTM(100, return_sequences=True))
+# model.add(LSTM(100))
+# model.add(Dense(100, activation='relu'))
+# model.add(Dense(vocab_size, activation='softmax'))
+# print(model.summary())
+#
+#
+# # compile model
+# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# # fit model
+# model.fit(X, y, batch_size=128, epochs=5)
+# # compile model
+# model.save('model.h5')
+# save the tokenizer
+dump(tokenizer, open('tokenizer.pkl', 'wb'))
+
--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ import json
 import os
 import codecs
 import unicodedata
+import string
 
 
 def save_all_data():
@@ -27,7 +28,57 @@ def save_all_data():
     data.close()
 
 
+def load_doc(filename):
+    file = open(filename, 'rb')
+    text = file.read().decode()
+    file.close()
+    return text
 
 
+def clean_doc(doc):
+    doc = doc.replace('--', ' ')
+    tokens = doc.split()
+    table = str.maketrans('', '', string.punctuation)
+    tokens = [w.translate(table) for w in tokens]
+    tokens = [word for word in tokens if word.isalpha()]
+    tokens = [word.lower() for word in tokens]
+    return tokens
 
 
+def create_sequences(tokens):
+    length = 50 + 1
+    sequences = list()
+    for i in range(length, len(tokens)):
+        seq = tokens[i-length:i]
+        line = ' '.join(seq)
+        sequences.append(line)
+    return sequences
+
+
+def save_doc(lines, filename):
+    data = '\n'.join(lines)
+    file = codecs.open(filename, 'w', encoding='utf-8')
+    file.write(data)
+    file.close()
+
+
+def split_data(data, split_ratio):
+    ln = len(data)
+    splitted_data = []
+    for i in range(0, ln//split_ratio):
+        splitted_data.append(data[i])
+
+    return splitted_data
+
+"""
+data = load_doc("all_data.txt").split('\n')
+
+splitted_data = split_data(data, 4)
+save_doc(splitted_data, "quarter_data.txt")
+
+splitted_data = split_data(data, 2)
+save_doc(splitted_data, "half_data.txt")
+
+splitted_data = split_data(data, 10)
+save_doc(splitted_data, "a_tenth_data.txt")
+"""
